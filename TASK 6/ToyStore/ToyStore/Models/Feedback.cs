3826d134fefb384d7c7184be13ee8274c93269b0
Berin <bmasovic1@etf.unsa.ba>
2023-04-29 18:40:22 +0200

    Projekat - klase

---
+++ TASK 6/ToyStore/ToyStore/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace ToyStore.Models
+{
+    public class Feedback
+    {
+        public string Id { get; }
+        public int Ocjena { get; set; }
+        public string Komentar { get; set; }
+        public Korisnik Korisnik { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
