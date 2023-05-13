e02266191c8ec1ab3da116cf2c8ef365a48cbf13
Berin <bmasovic1@etf.unsa.ba>
2023-05-13 19:49:58 +0200

    Dijagram klasa - popravke

---
+++ TASK 6/ToyStore/ToyStore/Models/Kartica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ToyStore.Models
+{
+    public class Kartica
+    {
+        public string Id { get; }
+        public int BrojKartica { get; set; }
+        public int MjesecIsteka { get; set; }
+        public int GodinaIsteka { get; set; }
+        public int Cvv { get; set; }
+    }
+}
---
+++ TASK 6/ToyStore/ToyStore/Models/Naplata.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ToyStore.Models
+{
+    public class Naplata
+    {
+        public string Id { get; }
+        public RegistrovaniKorisnik RegKorisnik { get; set; }
+        public Kartica Kartica { get; set; }
+        public Narudzba Narudzba { get; set; }
+        public Boolean Placeno { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
